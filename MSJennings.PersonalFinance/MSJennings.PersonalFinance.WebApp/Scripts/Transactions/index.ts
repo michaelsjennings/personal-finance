4a74494bbfe9741b2cdfb47a771f64c8ec4a0493
Michael Jennings <mijennin@microsoft.com>
2020-02-12 21:27:36 -0500

    Add typescript

---
+++ MSJennings.PersonalFinance/MSJennings.PersonalFinance.WebApp/Scripts/Transactions/index.ts
@@ -0,0 +1,11 @@
+﻿export const onLoad = (): void => {
+    addEventHandlers()
+}
+
+const addEventHandlers = (): void => {
+    $(document).off('click.deleteTransaction', '.deleteTransaction').on('click.deleteTransaction', '.deleteTransaction', deleteTransactionClick)
+}
+
+const deleteTransactionClick = (): void => {
+    console.log('deleteTransactionClick')
+}
