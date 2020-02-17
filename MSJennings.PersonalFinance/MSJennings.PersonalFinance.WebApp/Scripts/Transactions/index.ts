db718ca8bf6a2b7b64678a428c9b80e915757256
Michael Jennings <mijennin@microsoft.com>
2020-02-17 18:31:18 -0500

    Implement filter and clear buttons

--- MSJennings.PersonalFinance/MSJennings.PersonalFinance.WebApp/Scripts/Transactions/index.ts
+++ MSJennings.PersonalFinance/MSJennings.PersonalFinance.WebApp/Scripts/Transactions/index.ts
@@ -3,7 +3,17 @@
 }
 
 const addEventHandlers = (): void => {
+    $('#filterButton').off('click.filterButton').on('click.filterButton', filterButtonClick)
+    $('#clearButton').off('click.clearButton').on('click.clearButton', clearButtonClick)
     $(document).off('click.deleteTransaction', '.deleteTransaction').on('click.deleteTransaction', '.deleteTransaction', deleteTransactionClick)
+}
+
+const filterButtonClick = (): void => {
+    $.blockUI()
+}
+
+const clearButtonClick = (ev: Event): void => {
+    $(ev.target).closest('form').find(':input').val('')
 }
 
 const deleteTransactionClick = (ev: Event): void => {
