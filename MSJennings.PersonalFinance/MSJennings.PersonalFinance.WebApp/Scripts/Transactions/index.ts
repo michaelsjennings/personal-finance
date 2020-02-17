e1edaa96afae232e55a07d447c8af43056360a21
Michael Jennings <mijennin@microsoft.com>
2020-02-17 15:57:35 -0500

    Add delete confirmation

--- MSJennings.PersonalFinance/MSJennings.PersonalFinance.WebApp/Scripts/Transactions/index.ts
+++ MSJennings.PersonalFinance/MSJennings.PersonalFinance.WebApp/Scripts/Transactions/index.ts
@@ -6,6 +6,11 @@ const addEventHandlers = (): void => {
     $(document).off('click.deleteTransaction', '.deleteTransaction').on('click.deleteTransaction', '.deleteTransaction', deleteTransactionClick)
 }
 
-const deleteTransactionClick = (): void => {
-    console.log('deleteTransactionClick')
+const deleteTransactionClick = (ev: Event): void => {
+    bootbox.confirm('Are you sure you want to delete this item?', (result: boolean) => {
+        if (result) {
+            $.blockUI()
+            $(ev.target).closest('form').submit()
+        }
+    })
 }
